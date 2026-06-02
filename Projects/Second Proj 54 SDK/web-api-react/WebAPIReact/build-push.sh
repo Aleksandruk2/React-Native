84a6fc368017a5b56cc63881ff692bfe1d3bf562
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-06-02 17:28:52 +0300

    fix .sh file

--- Projects/Second Proj 54 SDK/web-api-react/WebAPIReact/build-push.sh
+++ Projects/Second Proj 54 SDK/web-api-react/WebAPIReact/build-push.sh
@@ -2,10 +2,10 @@
 
 # ==== API ====
 
-cd Second Proj 54 SDK\web-api-react\WebAPIReact\WebAPIReact
+cd WebAPIReact
 
 docker build -t web-api-react-native .
 docker tag web-api-react-nativei:latest avalentyn/web-api-react-native:latest
 docker push avalentyn/web-api-react-native:latest
 
-echo "DONE"
+echo "API DONE"
