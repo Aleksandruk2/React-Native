df95415b8ddd543f8b3e24a2a171fd33ca09893a
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-06-13 16:05:39 +0300

    added Chat tables

--- Projects/Second Proj 54 SDK/web-api-react/WebAPIReact/WebAPIReact/Entities/Identity/UserEntity.cs
+++ Projects/Second Proj 54 SDK/web-api-react/WebAPIReact/WebAPIReact/Entities/Identity/UserEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using WebAPIReact.Entities.Chat;
 
 namespace WebAPIReact.Entities.Identity;
 
@@ -10,5 +11,8 @@ public class UserEntity : IdentityUser<long>
     public string? LastName { get; set; } = string.Empty;
     public string? Image { get; set; } = string.Empty;
     public virtual ICollection<UserRoleEntity>? UserRoles { get; set; }
+    public virtual ICollection<ChatUserEntity>? ChatUsers { get; set; }
+    public virtual ICollection<ChatMessageEntity>? Messages { get; set; }
+    public virtual ICollection<ChatMessageReadEntity>? MessageReads { get; set; }
 
 }
